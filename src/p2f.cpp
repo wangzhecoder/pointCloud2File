0000ca2dba157c4771435868f881f9cd438325ab
wz <wangzhe936@outlook.com>
2017-07-24 16:30:31 +0800

    changed from save one file to series

--- src/p2f.cpp
+++ src/p2f.cpp
@@ -10,6 +10,9 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/io/ply_io.h>
+#include <iostream>
+#include <string>
+
 class p2f{
 	public:
 		p2f();
@@ -19,10 +22,11 @@ class p2f{
 		ros::NodeHandle _nh;
 		ros::Subscriber rslidar_pointCloud2_sub;
 		ros::Subscriber velodyne_pointCloud2_sub;
-
+		long frameNum;
 };
 
 p2f::p2f()
+	:frameNum(0)
 {
 	rslidar_pointCloud2_sub = _nh.subscribe("rslidar_points",1,&p2f::rslidar_pointCloud2File,this);
 	velodyne_pointCloud2_sub = _nh.subscribe("velodyne_points",1,&p2f::velodyne_pointCloud2File,this);
@@ -30,10 +34,19 @@ p2f::p2f()
 
 void p2f::rslidar_pointCloud2File(const sensor_msgs::PointCloud2ConstPtr & cloud_msg)
 {
-	pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
-	pcl_conversions::toPCL(*cloud_msg,*cloud);
-	pcl::io::savePLYFile("myPLY_rslidar.ply",*cloud);
-	ROS_INFO("saved");
+	if(frameNum%100 == 0)
+	{
+		pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
+		pcl_conversions::toPCL(*cloud_msg,*cloud);
+		std::string fileNames = "rslidar-";
+		std::stringstream ss;
+		std::string num;
+		ss<<frameNum;ss>>num;
+		fileNames = fileNames+num+".ply";
+		pcl::io::savePLYFile(fileNames,*cloud);
+		ROS_INFO("rslidar saved");
+	}
+	frameNum++;
 }
 
 void p2f::velodyne_pointCloud2File(const sensor_msgs::PointCloud2ConstPtr & cloud_msg)
@@ -41,7 +54,7 @@ void p2f::velodyne_pointCloud2File(const sensor_msgs::PointCloud2ConstPtr & clou
 	pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
 	pcl_conversions::toPCL(*cloud_msg,*cloud);
 	pcl::io::savePLYFile("myPLY_velodyne.ply",*cloud);
-	ROS_INFO("saved");
+	ROS_INFO("velodyne saved");
 }
 
 int main(int argc,char** argv)
